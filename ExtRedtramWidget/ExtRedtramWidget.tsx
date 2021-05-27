ebae75ac9d3bf252a612037ed7da22c7ecce00d1
redtram-tech <84892722+redtram-tech@users.noreply.github.com>
2021-05-27 09:25:35 +0300

    Add files via upload

---
+++ ExtRedtramWidget/ExtRedtramWidget.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { ExtEmbed } from '../ExtEmbed/ExtEmbed';
+
+interface IProps {
+    'data-blockid': number;
+    'data-ver' : string;
+    'data-width'?: string;
+    'data-height'?: string;
+}
+
+export function ExtRedtramWidget(props: IProps): React.ReactNode {
+    const {
+        'data-blockid': blockId,
+        'data-ver': ver,
+        'data-width': width = '100%',
+        'data-height': height = '300'
+    } = props;
+
+    const html = `<div class="r${Number(blockId)}" id="abba" idn="${Number(blockId)}"></div>` +
+        `<script async src="https://${String(ver)}.redtram.com/j/${Number(blockId)}/"></script>`;
+
+
+    return (
+        <div>
+            <ExtEmbed
+                html={html}
+                iframeHeight={height}
+                iframeWidth={width}
+            />
+        </div>
+    );
+}
